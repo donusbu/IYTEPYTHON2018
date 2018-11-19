6524ab76025136dcd20c7598c7eb9d6d4373ef2c
Duygu <44569786+perperunas@users.noreply.github.com>
2018-11-19 22:31:12 +0300

    Delete Q2.py

--- Q2.py
+++
@@ -1,9 +0,0 @@
-text = input("Bir text giriniz:")
-text.split()
-reverse = reversed(text)
-new_text = ''.join(reverse)
-
-if text == new_text:
-    print("Bu text palindromdur.")
-else:
-    print("Bu text palindrom değildir.")
